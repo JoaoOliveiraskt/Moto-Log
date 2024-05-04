0c3ee4fc97dd0d4d587ac07f6f83d1a4b07308ab
João Oliveira <joaoliveiraa7@gmail.com>
2024-05-04 11:12:27 -0300

    feat: Add calculateTotalPrice helper function for calculating total price

---
+++ src/app/helpers/price.ts
@@ -0,0 +1,12 @@
+import { Produto } from "prisma/generated/client";
+
+export const calculateTotalPrice = (product: Produto) => {
+    if (Number(product.porcentagemDesconto) === 0) {
+        return Number(product.preco);
+    }
+
+    const discount = Number(product.preco) * (Number(product.porcentagemDesconto) / 100);
+    return Number(product.preco) - discount;
+};
+
+export default calculateTotalPrice;
