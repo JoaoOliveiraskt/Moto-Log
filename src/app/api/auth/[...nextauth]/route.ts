14b1c3de8624d1933e5ff9ef5889ca3743b2e872
João Oliveira <joaoliveiraa7@gmail.com>
2024-06-21 08:52:01 -0300

    chore: Update NextAuth configuration with Prisma adapter and Google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,25 @@
-import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+    adapter: PrismaAdapter(db) as Adapter,
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        checks: ["none"],
+      }),
+    ],
+    callbacks: {
+      async session({ session, user }) {
+        session.user = { ...session.user, id: user.id };
+        return session;
+      },
+    },
+    secret: process.env.NEXTAUTH_SECRET as string,
+  });
 
 export { handler as GET, handler as POST };
