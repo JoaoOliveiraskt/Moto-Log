68b5ecf34d85f67d861a470e88d51389f27a4025
João Oliveira <joaoliveiraa7@gmail.com>
2024-08-09 21:25:47 -0300

    Update theme switcher button variant to "ghost"

--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -28,7 +28,7 @@ export function ModeToggle({className, children}: Props) {
       <DropdownMenuTrigger asChild className="text-muted-foreground hover:text-foreground ">
         <Button
           onClick={toggleTheme}
-          variant="icon"
+          variant="ghost"
           size="icon"
           className={`cursor-pointer outline-none border-none ${className}`}
         >
