47170bb04997fccbe1d5536931f92f73e66ffdeb
João Oliveira <joaoliveiraa7@gmail.com>
2024-10-17 09:07:32 -0300

    style: update login dialog layout

--- src/components/login-dialog.tsx
+++ src/components/login-dialog.tsx
@@ -13,7 +13,7 @@ interface Props {
 export default function LoginDialog({ open, onOpenChange }: Props) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="overflow-hidden max-w-md px-4 sm:max-w-2xl h-[50%] sm:h-[40%] sm:p-0 sm:rounded-3xl">
+      <DialogContent className="overflow-hidden max-w-[94%] px-4 md:max-w-2xl h-[50%] sm:h-[40%] sm:p-0 sm:rounded-3xl">
         <div className="mx-auto w-full h-full flex flex-col justify-between items-center">
           <div className="flex flex-col items-center space-y-4">
             <MotoLogLogo />
@@ -27,7 +27,7 @@ export default function LoginDialog({ open, onOpenChange }: Props) {
             </div>
           </div>
           <div className="flex flex-col justify-center gap-4 items-center border-t w-full py-4 min-h-36">
-            <LoginButton className="py-6 px-16 bg-card text-foreground border hover:bg-accent" />
+            <LoginButton className="py-6 lg:px-16 bg-card text-foreground border hover:bg-accent" />
           </div>
         </div>
       </DialogContent>
