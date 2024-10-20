f18aa6ccbe109391077530ddb11a852362c7da0a
João Oliveira <joaoliveiraa7@gmail.com>
2024-10-20 20:22:16 -0300

    style: update header and container layouts

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -8,7 +8,7 @@ export default function Container({ children, className, id }: ContainerProps) {
   return (
     <div
       id={`${id}`}
-      className={`${className} mx-auto max-w-screen-lg lg:max-w-[1480px] px-4`}
+      className={`${className} mx-auto max-w-screen-lg lg:max-w-[1480px] px-4 md:px-12`}
     >
       {children}
     </div>
