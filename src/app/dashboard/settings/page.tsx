8c4c94095d4135eed0330cd0351909dea52ea919
João Oliveira <joaoliveiraa7@gmail.com>
2024-12-27 22:00:03 -0300

    style: update layout settings page

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import Container from "@/components/container";
+
+import TypographyH4 from "@/components/typography/typography-h4";
+import TypographyP from "@/components/typography/typography-p";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Upload } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -69,63 +69,119 @@ export default function EditStore() {
   }, [selectedStoreId]);
 
   return (
-    <main className="flex flex-col space-y-10 mt-4">
+    <main>
       {selectedStore && (
-        <Card x-chunk="dashboard-07-chunk-0">
-          <CardHeader className="px-6 mt-6 space-y-2 mb-5">
-            <CardTitle className="text-3xl">Atualize sua loja</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-6">
-              <div className="grid gap-3">
+        <div className="max-w-4xl">
+          <TypographyH4 className="mb-8">Personalização da loja</TypographyH4>
+
+          <form className="grid gap-8" onSubmit={(e) => e.preventDefault()}>
+            <div className="grid gap-8">
+              <div className="grid gap-4">
                 <Label htmlFor="name">Nome</Label>
                 <Input
                   id="name"
                   type="text"
-                  className="max-w-xl"
+                  className=""
                   defaultValue={selectedStore.nome}
                 />
               </div>
-              <div className="grid gap-3">
+              <div className="grid gap-4">
                 <Label htmlFor="description">Descrição</Label>
                 <Textarea
                   id="description"
                   defaultValue={selectedStore.descricao}
-                  className="min-h-32 max-w-xl"
+                  className="min-h-32"
                 />
               </div>
             </div>
 
-            <div className="grid gap-3 relative h-fit mt-6">
-              <p>Imagem de banner</p>
-              <Label
-                htmlFor="storeImage"
-                className="flex items-center justify-center max-w-xl h-20 border border-dashed rounded-md cursor-pointer"
-              >
-                <span className="sr-only">Adicionar imagem de banner</span>
-                <Input
-                  id="storeImage"
-                  type="file"
-                  accept="image/*"
-                  className="absolute inset-0 opacity-0 cursor-pointer"
-                />
-
-                <Image
-                  alt="Preview"
-                  width={200}
-                  height={100}
-                  className="w-full h-full object-cover rounded-md"
-                  src={selectedStore.imagemUrl || "/placeholder-image.png"}
-                />
-
-                <div className="flex items-center justify-center w-full h-full">
-                  <Upload className="h-6 w-6 text-muted-foreground" />
+            <div className="grid gap-8">
+              {/* Imagem de banner */}
+              <div className="grid gap-4 relative h-fit">
+                <Label htmlFor="storeImage">Imagem de banner</Label>
+                <div className="flex flex-wrap gap-4">
+                  <Image
+                    alt="Preview Banner"
+                    width={200}
+                    height={100}
+                    className="w-full max-w-72 h-44 object-cover rounded-md"
+                    src={selectedStore.imagemUrl || "/placeholder-image.png"}
+                  />
+                  <div className="flex flex-col gap-y-4">
+                    <TypographyP className="text-muted-foreground text-sm max-w-xs">
+                      Para garantir os melhores resultados em todos os
+                      dispositivos, escolha uma imagem de até 6 MB com pelo
+                      menos 2.048 x 1.152 pixels.
+                    </TypographyP>
+                    <div className="h-fit w-fit">
+                      <input
+                        id="fileInput"
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={(e) => console.log(e.target.files)}
+                      />
+                      <Button
+                        type="button"
+                        variant={"secondary"}
+                        onClick={() =>
+                          document.getElementById("fileInput")?.click()
+                        }
+                      >
+                        Enviar
+                      </Button>
+                    </div>
+                  </div>
                 </div>
-              </Label>
+              </div>
+
+              {/* Imagem de perfil */}
+              <div className="grid gap-4 relative h-fit">
+                <Label htmlFor="profileImage">Imagem de perfil</Label>
+
+                <div className="flex flex-wrap gap-4">
+                  <div className="w-full max-w-72 h-44 bg-accent rounded-md flex items-center justify-center py-4">
+                    <Image
+                      alt="Preview Perfil"
+                      width={200}
+                      height={100}
+                      className="w-36 h-full object-cover rounded-full"
+                      src={selectedStore.imagemUrl || "/placeholder-image.png"}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-y-4">
+                    <TypographyP className="text-muted-foreground text-sm max-w-xs">
+                      Recomendamos usar uma imagem de até 4 MB com pelo menos 98
+                      x 98 pixels. Use um arquivo no formato PNG ou GIF (sem
+                      animações). E não se esqueça: as imagens precisam seguir
+                      as diretrizes da comunidade do Moto Log.
+                    </TypographyP>
+                    <div className="h-fit w-fit">
+                      <input
+                        id="fileInput"
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={(e) => console.log(e.target.files)}
+                      />
+                      <Button
+                        type="button"
+                        variant={"secondary"}
+                        onClick={() =>
+                          document.getElementById("fileInput")?.click()
+                        }
+                      >
+                        Enviar
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <Button className="mt-6">Atualizar</Button>
-          </CardContent>
-        </Card>
+
+            <Button className="w-fit">Atualizar</Button>
+          </form>
+        </div>
       )}
     </main>
   );
